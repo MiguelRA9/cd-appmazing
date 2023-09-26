2f9bc2daff7223955654d0e4febd1c7ace97d566
MiguelRA9 <miguel.ricoy.astray@gmail.com>
2023-09-26 12:22:45 +0200

    Added DTO for contact

---
+++ src/main/java/com/campusdual/appmazing/model/dto/ContactDto.java
@@ -0,0 +1,59 @@
+package com.campusdual.appmazing.model.dto;
+
+public class ContactDto {
+
+    private int id;
+    private String name;
+    private String first_surname;
+    private String second_surname;
+    private String phone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirst_surname() {
+        return first_surname;
+    }
+
+    public void setFirst_surname(String first_surname) {
+        this.first_surname = first_surname;
+    }
+
+    public String getSecond_surname() {
+        return second_surname;
+    }
+
+    public void setSecond_surname(String second_surname) {
+        this.second_surname = second_surname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
